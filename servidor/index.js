a607ebd7ddab4af98a3fda46dcee7d7425fb0bc2
Marcelo Goto <gotomarcelo98@gmail.com>
2021-12-28 15:28:39 -0300

    first commit

---
+++ servidor/index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+app.use(express.json());
+app.use(cors());
+
+const FoodModel = require("./models/Food.js");
+
+mongoose.connect(
+  "mongodb+srv://admin:admin@cluster0.zh8gi.mongodb.net/food?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+app.post("/insert", async (req, res) => {
+  const foodName = req.body.foodName;
+  const days = req.body.days;
+
+  const food = new FoodModel({ foodName: foodName, daysSinceIAte: days });
+
+  try {
+    await food.save();
+    res.send("Dados Inseridos");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/read", async (req, res) => {
+  FoodModel.find({}, (error, result) => {
+    if (error) {
+      res.send(error);
+    }
+
+    res.send(result);
+  });
+});
+
+app.put("/update", async (req, res) => {
+  const newFoodName = req.body.newFoodName;
+  const id = req.body.id;
+
+  try {
+    await FoodModel.findById(id, (error, updatedFood) => {
+      updatedFood.foodName = newFoodName;
+      updatedFood.save();
+      res.send("Atualizado");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+
+  await FoodModel.findByIdAndRemove(id).exec();
+  res.send("deletado")
+});
+
+app.listen(3001, () => {
+  console.log("Servidor rodando na porta 3001.");
+});
